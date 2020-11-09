940b84d449bb617939607dabb5e528f782287aa1
Heran Gao <herangao6@gmail.com>
2020-11-09 15:56:59 -0500

    add tabs directory, created Things.js and Services.js

---
+++ tabs/Services.js
@@ -0,0 +1,36 @@
+/**
+ Services tab contains a filter to show services of all or a subset of the things
+ (that is shows the union of services belonging to 4, 3, 2 or 1 things).
+ Each service presented under this tab should contain an identity of its owner thing.
+ **/
+
+/**
+ a prototype defines a service
+ */
+let Service = {
+    thing: null,
+    // todo
+}
+
+/**
+ A list contains all services
+ */
+services = []
+
+/**
+ * parse services into Service instances and add to the list
+ * @param   {?}
+ * @returns {?}
+ */
+const parseServices = (param) => {
+
+};
+
+/**
+ * filter services that belong to certain things
+ * @param   {list} a list of things whose services will be displayed
+ * @returns {list} a list of services that will be displayed
+ */
+const filter = (things) => {
+
+};
---
+++ tabs/Things.js
@@ -0,0 +1,12 @@
+/**
+ The Things tab includes info about each thing
+ **/
+
+/**
+ * get info for all things
+ * @param   {?}    ?
+ * @returns {?}    info for all things
+ */
+const getInfo = (param) => {
+
+};
