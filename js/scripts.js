9a00361f953dc741c00206389b4ca3ee23b06f23
Andrew Ratz <ar4757@gmail.com>
2020-11-16 21:35:52 +0000

    Added progress bar to accurately show when tweets are finished loading to the user. Also replaced old index.html with Cindy's layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //Not yet working, but the goal is to load all of our group's tweets here
 //We will use them to fill out our tabs with the data we receive
 function load() {
+	move();
+
 	$.ajax({
  	type: 'GET',
  	url: 'http://10.254.254.64:3000/tweets',
@@ -16,4 +18,28 @@ function load() {
    	console.log(xhr.responseText);
  	}
 	});
+}
+
+var i = 0;
+function move() {
+	if (i == 0) {
+		i = 1;
+		var elem = document.getElementById("bar");
+		var width = 0;
+		//Every 10 milliseconds update the bar
+		var id = setInterval(frame, 10);
+		function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+			i = 0;
+			var loadingBlockade = document.getElementById("loadingBlockade");
+			loadingBlockade.style.opacity = "0";
+			loadingBlockade.style.zIndex = "-1";
+		} else {
+			//Increment by 0.05 every 10 milliseconds. This means the bar will fill after 20 seconds
+			width += 0.05;
+			elem.style.width = width + "%";
+		}
+		}
+	}
 }
