9fe5a6eed4482c64197f66fcb484291d5149a6a4
Heran Gao <herangao6@gmail.com>
2020-11-21 11:37:11 -0500

    update recipe.js

--- tabs/recipe.js
+++ tabs/recipe.js
@@ -3,46 +3,25 @@ import {filteredServices_list} from "./Services"
 
 let recipe_list = []
 
-let current_func = checkIsRelationship;
 
-function checkIsRelationship(param){
+function putRelationship(param){
     if(param.type == "servicesRelationship") {
+        if(!param.first_service.is_bounded || ! param.second_service.is_bounded) {
+            console.error("there is unbounded service in this relationship");
+            return false;
+        }
         recipe_list.push(param);
-        checkIsFirstService.serviceRelationship = param;
-        return checkIsFirstService;
+        return true;
     }
-    else {
-        console.log("require a relationship here");
-    }
+    return false;
 
 }
 
-function checkIsFirstService(serviceName) {
-    if(checkIsFirstService.serviceRelationship.first_service.name == serviceName) {
-        return checkIsSecondService;
-    }
-    else {
-        console.log("First service does not match");
-    }
-
+function putService(service) {
+    recipe_list.push(service);
 }
 
-function checkIsSecondService(serviceName) {
-    if(checkIsFirstService.serviceRelationship.first_service.name == serviceName) {
-        return checkIsRelationship;
-    }
-    else {
-        console.log("Second service does not match");
-    }
-
-}
-
-
-const putNext = (param_list) => {
-    recipe_list = [];
-    param_list.forEach(param => current_func(param))
-    if(current_func != checkIsRelationship)
-        console.error("the last input isn't second service!");
-};
 
 exports.recipe_list = recipe_list
+exports.putRelationship = putRelationship
+exports.putService = putService
